bc943436acfc18e7d2416b2c795f6532c376b050
Marco Simon Zillgen <marcosimonzillgen@gmail.com>
2025-02-20 15:06:30 +0100

    i

--- components/Sidebar/BigDayNumber.tsx
+++ components/Sidebar/BigDayNumber.tsx
@@ -8,7 +8,7 @@ export default function BigDayNumber() {
   } = useDayContext();
 
   return (
-    <div className="w-full relative overflow-hidden select-none py-[12rem] h-[16rem] cursor-pointer">
+    <div className="w-full relative overflow-hidden select-none py-[12rem] h-[16rem]">
       <div className="text-[16rem] text-center select-none font-bold absolute top-0 left-0 w-full">
         {getDayNumber(selectedDay)}
       </div>
